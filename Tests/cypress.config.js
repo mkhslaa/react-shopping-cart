90314eea51b702e8b6340b424026f25121b378a2
Manjeet Khalsa <manjeet.khalsa@yahoo.com>
2023-05-21 21:44:58 +0100

    remove support file from cypress config

--- Tests/cypress.config.js
+++ Tests/cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: false,
     fixturesFolder: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
